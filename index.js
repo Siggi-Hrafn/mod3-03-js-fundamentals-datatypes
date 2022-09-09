1f8abbd9680eeb3d1c06f206267a350b4158a498
siggihrafnthorkels <89144713+siggihrafnthorkels@users.noreply.github.com>
2022-09-09 12:50:03 +0000

    uploading js file

---
+++ index.js
@@ -0,0 +1,14 @@
+//###### Variable declaration ########
+var myName = "Name: Siggi Hrafn" + " (by var declaration)";
+let myJob = "Occupation: I´m a teacher" + " (by get declaration)";
+const myPet = "My cat is named Dita von Teese" + "(by const declaration)";
+//###### Variable declaration ########
+
+
+
+
+
+console.log("Hello world!");
+console.log(myName);
+console.log(myJob);
+console.log(myPet);
